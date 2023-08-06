be36e4eea3c6fe4073930a24804b2f3cd5fd8ba1
Karen Huamán <karmahucents@gmail.com>
2023-08-05 22:54:38 -0500

    add: creando primera escena 3d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+/* import { BoxGeometry, MeshBasicMaterial } from 'three'; */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container" style={{ width: "100%", height: "100vh" }}>
+      <Canvas>
+        <mesh>
+          <boxGeometry attach="geometry" args={[1, 1, 1]} /> {/* Usa attach="geometry" */}
+          <meshBasicMaterial attach="material" color={"#ff0000"} /> {/* Usa attach="material" */}
+        </mesh>
+      </Canvas>
     </div>
   );
 }
